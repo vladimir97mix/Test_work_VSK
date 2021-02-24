43a8fb21e674e2f36c1e343d5a83d4af731cd364
vladimir97mix <62235606+vladimir97mix@users.noreply.github.com>
2021-02-24 19:51:43 +0700

    Update pyTestWork.py

--- pyTestWork.py
+++ pyTestWork.py
@@ -1,4 +1,6 @@
 import os
+from docx import Document
+from docx.shared import Inches
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -30,16 +32,12 @@ def WebDriverInit(inWebDriverFullPath, inChromeExeFullPath, inExtensionFullPathL
 
 
 def FindElemets(webDriver):
-    # Переходим на нужную страницу
-    webDriver.get('https://yandex.ru/')
-    # Таймаут ожидания
-    wait = WebDriverWait(webDriver, 10)
+    webDriver.get('https://yandex.ru/')  # Переходим на нужную страницу
+    wait = WebDriverWait(webDriver, 10)  # Таймаут ожидания
     # Дожидаемся до возможности кликнуть на строку поиска
     element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input.input__control.input__input")))
-    # Отправляем запрос в поисковую строку
-    element.send_keys("Купить страховку")
-    # Нажимаем на ENTER
-    element.send_keys(Keys.RETURN)
+    element.send_keys("Купить страховку")  # Отправляем запрос в поисковую строку
+    element.send_keys(Keys.RETURN)  # Нажимаем на ENTER
 
     elementMain = webDriver.find_elements(By.CLASS_NAME, 'serp-item')  # Основные эелементы поиска (li)
     elementLink = webDriver.find_elements(By.CLASS_NAME, 'OrganicTitle-Link')  # Линки для перехода по ссылкам
@@ -65,8 +63,6 @@ def FindElemets(webDriver):
     for elem in elementLink:
         elem.click()
 
-
-
     # Создаем папку для скринов
     if not os.path.exists('screens'):
         os.makedirs('screens')
@@ -75,11 +71,24 @@ def FindElemets(webDriver):
 
     picNum = 1
     for tab in handles[1:]:
-        webDriver.switch_to_window(tab)  # Переключаемся по каждой вкладке
+        webDriver.switch_to_window(tab)  # Переключаемся по каждой вкладке, кроме первой - yandex.ru
         webDriver.get_screenshot_as_file('screens/scr' + str(picNum) + '.png')  # Делаем скрин в папку 'screens'
         picNum += 1
+
+    picNum -= 1
+    strNum = 1  # Нумерация заголовка
+    doc = Document()
+    for txtTitle, txtExt in zip (titleTextList, extendetTextLis):  # Циклл по спискам заголовков
+        p = doc.add_paragraph()  # Создаем новый абзац
+        run = p.add_run(strNum + ' ' + txtTitle)  # Добавляем текст
+        run.bold = True  # Делаем текст жирным
+        doc.add_paragraph(txtExt)  # Вставляем подзаголовок в новый абзац
+        doc.add_picture('screens/scr' + str(picNum) + '.png', width=Inches(4.0))  # Вставляем скриншот, шириной 4 дюйма
+        picNum -= 1  # Декримент скринов
+        strNum += 1  # Инкримент номера заголовка
+
+    doc.save('Отчет по ключевой фразе.rtf')
 
 inWebDriver = WebDriverInit(gWebDriverFullPath, gChromeExeFullPath, gExtensionFullPathList)
 
 FindElemets(inWebDriver)
-
